9d51102cf36e0cce46694ca66a648be33e6a8529
Animesh Dhengre <dhengreani18@gmail.com>
2020-09-14 21:11:38 +0530

    Added contains duplicate

---
+++ LeetCode/containsDup.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> count = new HashMap<>();
+        int counter =0;
+        for(int i=0;i<nums.length;i++) {
+            count.put(nums[i],counter++);    
+        }
+        
+        if(count.size() < nums.length) return true;
+        
+        return false;
+    }
+}
